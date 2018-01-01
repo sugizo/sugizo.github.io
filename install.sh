69d6db70d192ce300caedbf325ae80748abe5664
sugizo <steve.van.christie@gmail.com>
2018-01-01 11:19:34 +0700

    Initial Commit

--- install.sh
+++ install.sh
@@ -1,32 +1,42 @@
-# Install Package
-mkdir -p ~/project/coffee/roots
-cd ~/project/coffee/roots
-npm list roots || npm install roots
+#!/bin/sh
 
-# New Site
-#node_modules/.bin/roots new stifix
+start=$(date +%s)
+
+# Install Package
+mkdir -p ~/project/go/hugo
+cd ~/project/go/hugo
+npm list hugo-cli || npm install hugo-cli
+
+# Initalize Blog
+rm -rf ~/project/go/hugo/stifix
+mkdir -p ~/project/go/hugo/stifix
+#node_modules/.bin/hugo new site stifix
 
 # install app
-mkdir -p ~/project/coffee/roots/stifix/
-rm -rf ~/project/coffee/roots/stifix/*
-rsync -avuzr ~/Cloud/MEGA/Git/roots/stifix/* ~/project/coffee/roots/stifix/
+rm -rf ~/project/go/hugo/stifix/content/*
+rm -rf ~/project/go/hugo/stifix/layouts/*
+rm -rf ~/project/go/hugo/stifix/public/*
+rm -rf ~/project/go/hugo/stifix/static/*
+rsync -zavr ~/Cloud/MEGA/Git/hugo/stifix/ ~/project/go/hugo/stifix/
 
-# Install Prerequisites
-cd ~/project/coffee/roots/stifix
-npm install
+# Run Server
+cd ~/project/go/hugo/stifix
+../node_modules/.bin/hugo server -D
 
-# Run App
-cd ~/project/coffee/roots/stifix
-../node_modules/.bin/roots watch
-#npm start
+# Access Server via Browser
+#http://localhost:1313
 
-# Check App on Browser
-#open http://localhost:1111
-
-# Compile App
-cd ~/project/coffee/roots/stifix
-../node_modules/.bin/roots compile
-#npm compile
+# Generate Static Site Website on folder public
+cd ~/project/go/hugo/stifix
+../node_modules/.bin/hugo
 
 # Check generated files
-du -hsc ~/project/coffee/roots/stifix/public/
+du -hsc ~/project/go/hugo/stifix/public/
+
+# check public folder change external js and css into internal (save) then concat and minify it
+
+end=$(date +%s)
+diff=$(( $end - $start ))
+
+echo Duration = $diff Seconds
+echo Finished at = `date +%Y-%m-%d\ %H:%M:%S`
