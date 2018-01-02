755b916a66feb6f67931485ae5355d23cdf2201b
sugizo <steve.van.christie@gmail.com>
2018-01-02 13:04:00 +0700

    Initial Commit

--- models/db_schema_event_log.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-for table in db.tables:
-	if '_archive' not in table and table != 'auth_event':
-		db[table]._after_insert.append(partial(administration_event.after_insert_event, table = table) )
-		db[table]._after_update.append(partial(administration_event.after_update_event, table = table) )
-		db[table]._before_delete.insert(0, partial(administration_event.before_delete_event, table=table) )
