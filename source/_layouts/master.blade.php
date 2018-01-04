ea78feb4b2c2795d86063e7599139d264bf5b4e6
sugizo <steve.van.christie@gmail.com>
2018-01-05 05:35:43 +0700

    Initial Commit

---
+++ source/_layouts/master.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE HTML>
+<html lang="{{ $page->lang }}">
+<head>
+@yield('head')
+</head>
+<body>
+@yield('body')
+</body>
+</html>
---
+++ source/_partials/resume-objective.blade.php
@@ -0,0 +1,6 @@
+<div class=avoid_pagebreak>
+<h4>{{ $page->objective->title }}</h4>
+<p>
+	{{ $page->objective->description }}
+</p>
+</div>
