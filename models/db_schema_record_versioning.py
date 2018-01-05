db3be7668b3d8d906526d0c4ba2103bdbe00b801
sugizo <steve.van.christie@gmail.com>
2018-01-05 10:35:25 +0700

    Initial Commit

--- models/db_schema_record_versioning.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# enable_record_versioning
-auth.enable_record_versioning(db)
-
-"""
-# enable record versioning for each table that need record versioning
-db.address._enable_record_versioning()
-"""
